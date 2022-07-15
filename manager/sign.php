d55d1331c70f6fb74f4b1939d33b49a666be3b4c
Habin <108309664+HABINOH@users.noreply.github.com>
2022-07-15 17:01:31 +0900

    Add files via upload

---
+++ manager/sign.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <h2> 회원가입 </h2>
+    <p>
+        <lable for="nik">NICK NAME : </lable>  
+        <input type="text" name="user_nik">
+    </p>
+    <p>
+        <lable for="ID">ID : </lable>  
+        <input type="text" name="user_id">
+    </p>
+    <p>
+        <lable for="PASSWORD">PASSWORD : </lable>  
+        <input type="text" name="user_pass">
+    </p>
+    <p>
+        <lable for="PASSWORD">CHECK PASS : </lable>  
+        <input type="text" name="user_pass2">
+    </p>
+    <button type="submit"> 회원가입 완료 </button>
+    <a href=""> 로그인 페이지로 이동 </a>
+    
+
+
+
+</body>
+</html>
